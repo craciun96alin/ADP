f734d8c2c4637064bde2a1060215a745b7a9d1fe
craciun96alin <craciun96alin@gmail.com>
2018-04-25 07:45:20 +0300

    Importing project

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -29,40 +29,43 @@ namespace ConsoleApp1
 
                 if (com.Rank == 0)
                 {
-                    for (int j = 0; j < 10; j++)
-                    {
-                        localsum1 += array[j];
-                    }
 
-                    for (int i = 1; i<10; i++)
+
+                    Random rnd = new Random();
+                    int rand = rnd.Next(1, 100);
+                    int lider = 0;
+                    for (int i=1; i<10;i++)
                     {
-                        for (int j = 0; j < 10; j++)
+                        int rand1 = com.Receive<int>(i, 0);
+                        if (rand <= rand1)
                         {
-                            localArray[j]= array[j+10*i];
+                            lider = i;
+                            rand = rand1;
                         }
-
-                        
-                        com.Send<int[]>(localArray,i , 0);
-                        finalsum += com.Receive<int>(i, 0);
                     }
-                    finalsum += localsum1;
-                    
+
+                    for (int i = 1; i < 10; i++)
+                        com.Send<int>(lider, i, 0);
+
                 }
                 else
                 {
-                    com.Receive(0, 0, out int[] localarr);
-                    for (int j = 0; j < 10; j++)
-                    {
-                        localsum += localarr[j];
-                    }
-                    com.Send<int>(localsum, 0, 0);
+
+
+                    Random rnd = new Random();
+                    int rand = rnd.Next(1, 100); // trebuia sa fac altfel rand
+                    Console.WriteLine("Rand intoarce:" + rand);
+                    com.Send<int>(rand, 0, 0);
+
+                    com.Receive(0, 0,out int lider);
+                    Console.WriteLine("Liderul este" + lider);
                 }
 
                 com.Dispose();
                //     MPI.Communicator.world.Send(3, 0, 0);
             }
 
-            Console.WriteLine("Suma finala este:" + finalsum);
+            
         }
     }
 }
